a8d4dc0f4b8f48cfc6185b44083c2af0d1e0f309
Matthew Timoney <mtimoney@college.harvard.edu>
2022-07-29 01:02:55 -0400

    error handling

--- Messages/Spelling.cs
+++ Messages/Spelling.cs
@@ -4,16 +4,21 @@ using WeCantSpell.Hunspell;
 
 public class Spelling {
   public List<string> findEnglishWords(string possibleMessage) {
-    var dictionary = WordList.CreateFromFiles(@"English (American).dic");
+    try {
+      var dictionary = WordList.CreateFromFiles(@"English (American).dic");
 
-    List<string> englishWords = new List<string>();
-    string[] tokens = possibleMessage.Split(' ');
-    foreach (string token in tokens) {
-      if (token.Length > 1 && dictionary.Check(token)) {
-        englishWords.Add(token);
+      List<string> englishWords = new List<string>();
+      string[] tokens = possibleMessage.Split(' ');
+      foreach (string token in tokens) {
+        if (token.Length > 1 && dictionary.Check(token)) {
+          englishWords.Add(token);
+        }
       }
+      return englishWords;
+    } catch (Exception ex) {
+      Console.WriteLine($"error in findEnglishWords for message {possibleMessage}: {ex}");
+      return new List<string>();
     }
-    return englishWords;
   }
 
 }
