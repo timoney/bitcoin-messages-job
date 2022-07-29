1d428cf10d60808ec8b395a5df3c22a423d46833
Matthew Timoney <mtimoney@college.harvard.edu>
2022-07-28 23:54:41 -0400

    trigger build

--- Models/BlockchainMessage.cs
+++ Models/BlockchainMessage.cs
@@ -4,6 +4,7 @@ public class BlockchainMessage {
   public int blockHeight { get; set; }
   public string transactionId { get; set; }
   public string message { get; set; }
+  // only used for debuggin purposed right now. not persisted
   public List<string> validWords { get; set; }
 
   public BlockchainMessage(int blockHeight, string transactionId, string message, List<string> validWords) {
