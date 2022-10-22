e4a1c26f21a275987b4f75d994814e5c106baed4
Matt <timoney.matt@gmail.com>
2022-10-22 14:43:06 -0400

    change to utf-8 encoding and try to filter out messages that don't make sense

--- Messages/OpReturnSearch.cs
+++ Messages/OpReturnSearch.cs
@@ -8,20 +8,37 @@ public class OpReturnSearch {
 
   Spelling spelling = new Spelling();
 
+  List<String> dumbWords = new List<String>() {
+    "zT", "Fl"
+  };
+
+  // a hack for now. hopefully filter out messages that don't actually make any sense
+  private bool isNormalWord(List<String> words) {
+    var word = words[0];
+    var isInDumbWords = dumbWords.FirstOrDefault(stringToCheck => stringToCheck.Contains(word));
+    if (isInDumbWords != null) {
+      Console.WriteLine($"word: {word} is a dumb word!!");
+      return false;
+    }
+    return true;
+  }
+
   public string findMessage(string hexMessage) {
-    var message = Encoding.GetEncoding("ISO-8859-1").GetString(Convert.FromHexString(hexMessage));
+    //var encoding = "ISO-8859-1";
+    var encoding = "UTF-8";
+    var message = Encoding.GetEncoding(encoding).GetString(Convert.FromHexString(hexMessage));
     List<String> englishWords = spelling.findWords(message, DicLanguage.English);
-    if (englishWords.Count > 0) {
+    if (englishWords.Count > 0 && isNormalWord(englishWords)) {
       Console.WriteLine($"English words: {JsonConvert.SerializeObject(englishWords, Formatting.Indented)}");
       return message;
     }
     List<String> spanishWords = spelling.findWords(message, DicLanguage.Spanish);
-    if (spanishWords.Count > 0) {
+    if (spanishWords.Count > 0 && isNormalWord(spanishWords)) {
       Console.WriteLine($"Spanish words: {JsonConvert.SerializeObject(spanishWords, Formatting.Indented)}");
       return message;
     }
     List<String> frenchWords = spelling.findWords(message, DicLanguage.French);
-    if (frenchWords.Count > 0) {
+    if (frenchWords.Count > 0 && isNormalWord(frenchWords)) {
       Console.WriteLine($"French words: {JsonConvert.SerializeObject(frenchWords, Formatting.Indented)}");
       return message;
     }
