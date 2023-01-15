eacb60f385ca5f511a50d009f4276ac317aa9834
Matt <timoney.matt@gmail.com>
2023-01-15 12:26:01 -0500

    filter out more things

--- Messages/OpReturnSearch.cs
+++ Messages/OpReturnSearch.cs
@@ -9,7 +9,7 @@ public class OpReturnSearch {
   Spelling spelling = new Spelling();
 
   List<String> dumbWords = new List<String>() {
-    "zT", "Fl", "BERNSTEIN", "consolidate", "Hath"
+    "zT", "Fl", "aggregate", "consolidate", "Hath"
   };
 
   // a hack for now. hopefully filter out messages that don't actually make any sense
@@ -23,10 +23,21 @@ public class OpReturnSearch {
     return true;
   }
 
+  private bool isStupidMessage(String message) {
+    if (message.StartsWith("BERNSTEIN 2.0 REG")) {
+      Console.WriteLine($"stupid message");
+      return false;
+    } else {
+      return true;
+    }
+  }
+
   public string findMessage(string hexMessage) {
     //var encoding = "ISO-8859-1";
     var encoding = "UTF-8";
     var message = Encoding.GetEncoding(encoding).GetString(Convert.FromHexString(hexMessage));
+    if (isStupidMessage(message)) return null;
+
     List<String> englishWords = spelling.findWords(message, DicLanguage.English);
     if (englishWords.Count > 0 && isNormalWord(englishWords)) {
       Console.WriteLine($"English words: {JsonConvert.SerializeObject(englishWords, Formatting.Indented)}");
